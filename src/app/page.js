d61acb78a500de65e7324e514651aca706846d3a
divya-manirajan <divya.manirajan@gmail.com>
2025-04-14 17:29:12 -0500

    pushed all files

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,120 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { Info, Wind } from "lucide-react";
+import { useState } from "react";
+
+export default function page() {
+  const [location, setLocation] = useState("")
+  const [moreInfo, setMoreInfo] = useState(false)
+  const [results, setResults] = useState(
+    {
+      city: "",
+      region:"",
+      country:"",
+      time:"",
+
+      temp_f: "",
+      feelslike_f:"",
+      icon: "",
+      text:"",
+
+      cloud:"",
+      humidity:"",
+      heatindex_f: "",
+
+      wind_mph: "",
+      wind_degree: "",
+      wind_dir: "",
+      gust_mph:"",
+      windchill_f:"",
+    }
+  )
+
+  const getData = async (e) => {
+    e.preventDefault()
+    try{
+      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${location}`);
+      const data = await response.json()
+      console.log(data)
+      setResults(
+        {
+          city: data.location.name,
+          region: data.location.region,
+          country: data.location.country,
+          time: data.location.localtime,
+    
+          temp_f: data.current.temp_f,
+          feelslike_f: data.current.feelslike_f,
+          icon: data.current.condition.icon,
+          text: data.current.condition.text,
+    
+          cloud: data.current.cloud,
+          humidity: data.current.humidity,
+          heatindex_f: data.current.heatindex_f,
+
+          wind_mph: data.current.wind_mph,
+          wind_degree: data.current.wind_degree,
+          wind_dir: data.current.wind_dir,
+          gust_mph: data.current.gust_mph,
+          windchill_f: data.current.windchill_f
+        }
+      )
+    } catch (e) {
+      console.log(e)
+      alert("Please Enter Valid City")
+    }
+  }
+  const resultsEntries = Object.entries(results);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    
+    <div className="h-screen flex justify-center items-center bg-blue-200">
+      <div className="shadow-xl border-1 border-gray-100 rounded-2xl bg-white p-5"> 
+        <form className="h-full w-full rounded-2xl text-center" onSubmit={getData}>
+          <h1 className="font-serif font-semibold text-xl m-2">Weather Widget</h1>
+          <input className="font-serif border-1 border-gray-200 h-[30px] rounded-lg m-1 pl-2" placeholder="Enter City..." onChange={(e) => {setLocation(e.target.value)}}></input>
+          <button className="font-serif bg-black text-white h-[30px] rounded-lg m-l pl-2 pr-2">Submit</button>
+        </form>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="w-full flex flex-col items-center font-serif">
+            <p className="text-lg">{results.city}</p>
+
+            {results.temp_f && <p className="text-3xl mb-2">{results.temp_f}&deg;F</p>}
+            {results.feelslike_f && <p className="text-base">Feels Like: {results.feelslike_f}&deg;F</p>}
+
+            {results.icon && results.text && <div className="flex flex-row items-center">
+              <img src={results.icon}></img>
+              <p className="min-w-[64px] text-center">{results.text}</p>
+            </div>}
+
+            {results.city && <Info className="w-5 h-5 text-blue-500" onClick={() => setMoreInfo(!moreInfo)}/>}
+            
+            {moreInfo && (
+              <>
+                {/* {results.region && results.country && <p>{results.region}, {results.country}</p>} */}
+                <div id="wind-widget" className="w-full border-1 border-blue-200 shadow-xl rounded-4xl font-serif m-2 p-4">
+                  <div className="w-full flex items-center">
+                    <Wind className="h-5 w-5 mr-1" />
+                    <p className="text-xs">WIND</p>
+                  </div>
+                  <div className="w-full flex justify-between mt-2">
+                    <div className="flex flex-col">
+                      <p>Wind: </p>
+                      <p>Gusts: </p>
+                      <p>Direction: </p>
+                    </div>
+                      
+                    <div className="flex flex-col items-end">
+                      <p>{results.wind_mph} mph </p>
+                      <p>{results.gust_mph} mph</p>
+                      <p>{results.wind_degree}&deg; {results.wind_dir}</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
-  );
+  )
 }
